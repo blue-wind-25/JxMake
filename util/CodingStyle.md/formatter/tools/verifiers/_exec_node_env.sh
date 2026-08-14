2fca55fd13dce1281152453be445e201c0b67ec9
Aloysius Indrayanto <71955351+blue-wind-25@users.noreply.github.com>
2026-08-15 05:06:35 +0700

    Cleanups

--- util/CodingStyle.md/formatter/tools/verifiers/_exec_node_env.sh
+++ util/CodingStyle.md/formatter/tools/verifiers/_exec_node_env.sh
@@ -3,15 +3,14 @@
 # Common Node.js runtime environment.
 #
 
-NODE_HOME=/opt/node-v24.14.0-linux-x64
-MYNPM_HOME="$HOME/mynpm"
+: "${NODE_HOME:=/opt/node-v24.14.0-linux-x64}"
+: "${USER_NPM_HOME:=$HOME/mynpm}"
+: "${USER_LD_LIBRARY_PATH:=/opt/gcc-7.5.0/lib64:/opt/gcc-7.5.0/lib:/opt/isl-0.16.1/lib}"
 
-export LD_LIBRARY_PATH=/opt/gcc-7.5.0/lib64:/opt/gcc-7.5.0/lib:/opt/isl-0.16.1/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-
-export NODE_PATH="$NODE_HOME/lib/node_modules:$MYNPM_HOME/node_modules${NODE_PATH:+:$NODE_PATH}"
-
-export PATH="$NODE_HOME/bin:$MYNPM_HOME/bin:$PATH"
+export LD_LIBRARY_PATH=${USER_LD_LIBRARY_PATH}${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+export NODE_PATH="$NODE_HOME/lib/node_modules:$USER_NPM_HOME/node_modules${NODE_PATH:+:$NODE_PATH}"
+export PATH="$NODE_HOME/bin:$USER_NPM_HOME/bin:$PATH"
 
 NODE="$NODE_HOME/bin/node"
 NPM="$NODE_HOME/bin/npm"
-export NODE NPM MYNPM_HOME
+export NODE NPM USER_NPM_HOME

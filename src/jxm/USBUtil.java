b44e4d8786cca8152d0c656ec29a3da4e52d5da5
Aloysius Indrayanto <71955351+blue-wind-25@users.noreply.github.com>
2026-09-03 05:45:44 +0700

    Cleanups

--- src/jxm/USBUtil.java
+++ src/jxm/USBUtil.java
@@ -9,6 +9,7 @@ package jxm;
 
 
 import java.lang.reflect.Method;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -107,129 +108,125 @@ public class USBUtil  {
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    /*
-     * "java-does-usb" (net.codecrete.usb) is compiled for a class file version newer than what older JDKs'
-     * javac can parse (it relies on the modern Java Foreign Function & Memory API). Since this source tree is
-     * also compiled with older JDKs (see JxMakeFile's `make jar JDK_VER=8`), it cannot be referenced directly
-     * via import/compile-time types here - doing so would break the older-JDK compile. All access to it is
-     * therefore done purely via reflection, resolved lazily and cached; if the library's classes cannot be
-     * loaded (missing jar, or a JDK too old to even parse its class files at runtime), USB enumeration is
-     * silently disabled and getDevices() returns an empty list.
-     */
+    // NOTE : Use reflection to ensure this class can be compiled both with older and newer Java!
 
-    private static boolean reflectionInitialized;
-    private static boolean reflectionAvailable;
+    private static boolean _reflectionInitialized = false;
+    private static boolean _reflectionAvailable   = false;
 
-    private static Method usb_getDevices;
+    private static Method _usb_getDevices;
 
-    private static Method usbDevice_getVendorId;
-    private static Method usbDevice_getProductId;
-    private static Method usbDevice_getManufacturer;
-    private static Method usbDevice_getProduct;
-    private static Method usbDevice_getSerialNumber;
-    private static Method usbDevice_getClassCode;
-    private static Method usbDevice_getSubclassCode;
-    private static Method usbDevice_getProtocolCode;
-    private static Method usbDevice_getUsbVersion;
-    private static Method usbDevice_getDeviceVersion;
-    private static Method usbDevice_getInterfaces;
-    private static Method usbDevice_getInterface;
+    private static Method _usbDevice_getVendorId;
+    private static Method _usbDevice_getProductId;
+    private static Method _usbDevice_getManufacturer;
+    private static Method _usbDevice_getProduct;
+    private static Method _usbDevice_getSerialNumber;
+    private static Method _usbDevice_getClassCode;
+    private static Method _usbDevice_getSubclassCode;
+    private static Method _usbDevice_getProtocolCode;
+    private static Method _usbDevice_getUsbVersion;
+    private static Method _usbDevice_getDeviceVersion;
+    private static Method _usbDevice_getInterfaces;
+    private static Method _usbDevice_getInterface;
 
-    private static Method usbInterface_getAlternates;
-    private static Method usbInterface_getAlternate;
+    private static Method _usbInterface_getAlternates;
+    private static Method _usbInterface_getAlternate;
 
-    private static Method usbAlternateInterface_getEndpoints;
+    private static Method _usbAlternateInterface_getEndpoints;
 
-    private static synchronized boolean ensureReflectionInitialized()
+    private static synchronized boolean _ensureReflectionInitialized()
     {
-        if(reflectionInitialized) return reflectionAvailable;
+        if(_reflectionInitialized) return _reflectionAvailable;
 
-        reflectionInitialized = true;
-        reflectionAvailable   = false;
+        _reflectionInitialized = true;
+        _reflectionAvailable   = false;
 
         try {
 
-            final Class<?> usb_class                    = Class.forName("net.codecrete.usb.Usb"                   );
-            final Class<?> usbDevice_class               = Class.forName("net.codecrete.usb.UsbDevice"             );
-            final Class<?> usbInterface_class            = Class.forName("net.codecrete.usb.UsbInterface"          );
-            final Class<?> usbAlternateInterface_class   = Class.forName("net.codecrete.usb.UsbAlternateInterface" );
+            final Class<?> usb_class                   = Class.forName("net.codecrete.usb.Usb"                   );
+            final Class<?> usbDevice_class             = Class.forName("net.codecrete.usb.UsbDevice"             );
+            final Class<?> usbInterface_class          = Class.forName("net.codecrete.usb.UsbInterface"          );
+            final Class<?> usbAlternateInterface_class = Class.forName("net.codecrete.usb.UsbAlternateInterface" );
 
-            usb_getDevices                    = usb_class.getMethod("getDevices");
+            _usb_getDevices                     = usb_class                  .getMethod("getDevices");
 
-            usbDevice_getVendorId             = usbDevice_class.getMethod("getVendorId"     );
-            usbDevice_getProductId            = usbDevice_class.getMethod("getProductId"    );
-            usbDevice_getManufacturer         = usbDevice_class.getMethod("getManufacturer" );
-            usbDevice_getProduct              = usbDevice_class.getMethod("getProduct"      );
-            usbDevice_getSerialNumber         = usbDevice_class.getMethod("getSerialNumber" );
-            usbDevice_getClassCode            = usbDevice_class.getMethod("getClassCode"    );
-            usbDevice_getSubclassCode         = usbDevice_class.getMethod("getSubclassCode" );
-            usbDevice_getProtocolCode         = usbDevice_class.getMethod("getProtocolCode" );
-            usbDevice_getUsbVersion           = usbDevice_class.getMethod("getUsbVersion"   );
-            usbDevice_getDeviceVersion        = usbDevice_class.getMethod("getDeviceVersion");
-            usbDevice_getInterfaces           = usbDevice_class.getMethod("getInterfaces"   );
-            usbDevice_getInterface            = usbDevice_class.getMethod("getInterface", int.class);
+            _usbDevice_getVendorId              = usbDevice_class            .getMethod("getVendorId"                );
+            _usbDevice_getProductId             = usbDevice_class            .getMethod("getProductId"               );
+            _usbDevice_getManufacturer          = usbDevice_class            .getMethod("getManufacturer"            );
+            _usbDevice_getProduct               = usbDevice_class            .getMethod("getProduct"                 );
+            _usbDevice_getSerialNumber          = usbDevice_class            .getMethod("getSerialNumber"            );
+            _usbDevice_getClassCode             = usbDevice_class            .getMethod("getClassCode"               );
+            _usbDevice_getSubclassCode          = usbDevice_class            .getMethod("getSubclassCode"            );
+            _usbDevice_getProtocolCode          = usbDevice_class            .getMethod("getProtocolCode"            );
+            _usbDevice_getUsbVersion            = usbDevice_class            .getMethod("getUsbVersion"              );
+            _usbDevice_getDeviceVersion         = usbDevice_class            .getMethod("getDeviceVersion"           );
+            _usbDevice_getInterfaces            = usbDevice_class            .getMethod("getInterfaces"              );
+            _usbDevice_getInterface             = usbDevice_class            .getMethod("getInterface"    , int.class);
 
-            usbInterface_getAlternates        = usbInterface_class.getMethod("getAlternates"       );
-            usbInterface_getAlternate         = usbInterface_class.getMethod("getAlternate", int.class);
+            _usbInterface_getAlternates         = usbInterface_class         .getMethod("getAlternates"              );
+            _usbInterface_getAlternate          = usbInterface_class         .getMethod("getAlternate"    , int.class);
 
-            usbAlternateInterface_getEndpoints = usbAlternateInterface_class.getMethod("getEndpoints");
+            _usbAlternateInterface_getEndpoints = usbAlternateInterface_class.getMethod("getEndpoints"               );
 
-            reflectionAvailable = true;
+            _reflectionAvailable = true;
 
         }
         catch(final Throwable t) {
             // Library not present, or its class files cannot be loaded/parsed by this JVM - USB
-            // enumeration is disabled, getDevices() will simply report no devices.
-            reflectionAvailable = false;
+            // enumeration is disabled, getDevices() will simply report no devices
+            _reflectionAvailable = false;
         }
 
-        return reflectionAvailable;
+        return _reflectionAvailable;
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public static ArrayList<USBDevice> getDevices()
     {
         final ArrayList<USBDevice> devices = new ArrayList<>();
 
-        if( !ensureReflectionInitialized() ) return devices;
+        if( !_ensureReflectionInitialized() ) return devices;
 
         try {
 
-            final Collection<?> uDevs = (Collection<?>) usb_getDevices.invoke(null);
+            final Collection<?> uDevs = (Collection<?>) _usb_getDevices.invoke(null);
 
             for(final Object uDev : uDevs) {
 
-                final List<?> uInterfaces      = (List<?>) usbDevice_getInterfaces.invoke(uDev);
+                final List<?> uInterfaces      = (List<?>) _usbDevice_getInterfaces.invoke(uDev);
                 final int     numOfInterfaces  = uInterfaces.size();
                 final int[]   numOfAlternates  = new int[numOfInterfaces];
                 final int[][] numOfEndpoints   = new int[numOfInterfaces][];
 
                 for(int i = 0; i < numOfInterfaces; ++i) {
 
-                    final Object  uInterface = usbDevice_getInterface.invoke(uDev, i);
-                    final List<?> uAlternates = (List<?>) usbInterface_getAlternates.invoke(uInterface);
+                    final Object  uInterface = _usbDevice_getInterface.invoke(uDev, i);
+                    final List<?> uAlternates = (List<?>) _usbInterface_getAlternates.invoke(uInterface);
 
                     numOfAlternates[i] = uAlternates.size();
+                    numOfEndpoints [i] = new int[ numOfAlternates[i] ];
 
-                    numOfEndpoints[i] = new int[ numOfAlternates[i] ];
                     for(int j = 0; j < numOfAlternates[i]; ++j) {
-                        final Object  uAlternate = usbInterface_getAlternate.invoke(uInterface, j);
-                        final List<?> uEndpoints = (List<?>) usbAlternateInterface_getEndpoints.invoke(uAlternate);
+
+                        final Object  uAlternate = _usbInterface_getAlternate.invoke(uInterface, j);
+                        final List<?> uEndpoints = (List<?>) _usbAlternateInterface_getEndpoints.invoke(uAlternate);
                         numOfEndpoints[i][j] = uEndpoints.size();
-                    }
+
+                    } // for
 
                 } // for
 
                 devices.add( new USBDevice(
-                    (Integer) usbDevice_getVendorId     .invoke(uDev),
-                    (Integer) usbDevice_getProductId    .invoke(uDev),
-                    (String ) usbDevice_getManufacturer .invoke(uDev),
-                    (String ) usbDevice_getProduct      .invoke(uDev),
-                    (String ) usbDevice_getSerialNumber .invoke(uDev),
-                    (Integer) usbDevice_getClassCode    .invoke(uDev),
-                    (Integer) usbDevice_getSubclassCode .invoke(uDev),
-                    (Integer) usbDevice_getProtocolCode .invoke(uDev),
-                    String.valueOf( usbDevice_getUsbVersion   .invoke(uDev) ),
-                    String.valueOf( usbDevice_getDeviceVersion.invoke(uDev) ),
+                    (Integer)       _usbDevice_getVendorId     .invoke(uDev),
+                    (Integer)       _usbDevice_getProductId    .invoke(uDev),
+                    (String )       _usbDevice_getManufacturer .invoke(uDev),
+                    (String )       _usbDevice_getProduct      .invoke(uDev),
+                    (String )       _usbDevice_getSerialNumber .invoke(uDev),
+                    (Integer)       _usbDevice_getClassCode    .invoke(uDev),
+                    (Integer)       _usbDevice_getSubclassCode .invoke(uDev),
+                    (Integer)       _usbDevice_getProtocolCode .invoke(uDev),
+                    String.valueOf( _usbDevice_getUsbVersion   .invoke(uDev) ),
+                    String.valueOf( _usbDevice_getDeviceVersion.invoke(uDev) ),
                     numOfInterfaces,
                     numOfAlternates,
                     numOfEndpoints
@@ -240,7 +237,7 @@ public class USBUtil  {
         }
         catch(final Throwable t) {
             // Any reflective failure at call time (e.g. an API shape change) also degrades to "no devices"
-            // rather than crashing the caller.
+            // rather than crashing the caller
             devices.clear();
         }
 
